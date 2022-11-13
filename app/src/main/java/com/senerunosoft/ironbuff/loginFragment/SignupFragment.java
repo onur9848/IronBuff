9280d1e1a49084cd13b98af180d50d32a38bc8c2
onur9848 <61599460+onur9848@users.noreply.github.com>
2022-11-13 23:10:50 +0300

    sign up completed

--- app/src/main/java/com/senerunosoft/ironbuff/loginFragment/SignupFragment.java
+++ app/src/main/java/com/senerunosoft/ironbuff/loginFragment/SignupFragment.java
@@ -1,9 +1,7 @@
 package com.senerunosoft.ironbuff.loginFragment;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.*;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -17,73 +15,80 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.senerunosoft.ironbuff.R;
+import com.senerunosoft.ironbuff.databinding.FragmentSignupBinding;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 
 public class SignupFragment extends Fragment {
-
+    FirebaseAuth auth;
+    FirebaseFirestore firestore;
+    FragmentSignupBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false);
+        binding = FragmentSignupBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        return root;
     }
 
-    EditText namesurname, username, email, password;
-    FirebaseAuth auth;
-    FirebaseFirestore firestore;
-    Button registerbuton;
 
     @Override
     public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         tanimla(view);
-        registerButton();
 
+        binding.signupRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                registerButtonProcess();
+
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 
     private void tanimla(@NotNull View view) {
-        namesurname = view.findViewById(R.id.signup_namesurname);
-        username = view.findViewById(R.id.signup_username);
-        email = view.findViewById(R.id.signup_email);
-        password = view.findViewById(R.id.signup_password);
-        registerbuton = view.findViewById(R.id.register_button);
         auth = FirebaseAuth.getInstance();
         firestore = FirebaseFirestore.getInstance();
     }
 
-    private void registerButton() {
-        registerbuton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String namesurname_text, username_text, email_text, password_text;
-                if (checkField()){
-                    namesurname_text = namesurname.getText().toString();
-                    username_text = username.getText().toString();
-                    email_text = email.getText().toString();
-                    password_text = password.getText().toString();
-                    auth.createUserWithEmailAndPassword(email_text,password_text).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                        @Override
-                        public void onSuccess(AuthResult authResult) {
-                            HashMap<String, Object> userData = new HashMap<>();
-                            userData.put("NameSurname",namesurname_text);
-                            userData.put("UserName",username_text);
-                            userData.put("E-mail",email_text);
-                            firestore.collection("userTable").add(userData).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                                @Override
-                                public void onSuccess(DocumentReference documentReference) {
-                                    Toast.makeText(getContext(), "başarılı", Toast.LENGTH_SHORT).show();
-                                }
-                            }).addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull @NotNull Exception e) {
-                                    Toast.makeText(getContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-                                }
-                            });
+    private void registerButtonProcess() {
 
+
+        String namesurname_text, username_text, email_text, password_text, age_text, weight_text, sex_text;
+        if (checkField()) {
+            namesurname_text = binding.signupNamesurname.getText().toString();
+            username_text = binding.signupUsername.getText().toString();
+            email_text = binding.signupEmail.getText().toString();
+            password_text = binding.signupPassword.getText().toString();
+            age_text = binding.signupAge.getText().toString();
+            weight_text = binding.signupWeight.getText().toString();
+            sex_text = getSex();
+            binding.profilSexRadiogroup.getCheckedRadioButtonId();
+
+
+            auth.createUserWithEmailAndPassword(email_text, password_text).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    HashMap<String, Object> userData = new HashMap<>();
+                    userData.put("NameSurname", namesurname_text);
+                    userData.put("UserName", username_text);
+                    userData.put("E-mail", email_text);
+                    userData.put("Age", age_text);
+                    userData.put("Weight", weight_text);
+                    userData.put("Sex", sex_text);
+                    firestore.collection("userTable").add(userData).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Toast.makeText(getContext(), "başarılı", Toast.LENGTH_SHORT).show();
                         }
                     }).addOnFailureListener(new OnFailureListener() {
                         @Override
@@ -92,26 +97,41 @@ public class SignupFragment extends Fragment {
                         }
                     });
 
-
-
                 }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull @NotNull Exception e) {
+                    Toast.makeText(getContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
 
-            }
-        });
+
+        }
+
+    }
+
+    private String getSex() {
+
+        int i = binding.profilSexRadiogroup.getCheckedRadioButtonId();
+        RadioButton radioButton = binding.getRoot().findViewById(i);
+
+        return radioButton.getText().toString();
+
     }
 
     private boolean checkField() {
-        boolean is_namesurname, is_username, is_email, is_password;
-        is_namesurname = namesurname.getText().toString().isEmpty();
-        is_username = username.getText().toString().isEmpty();
-        is_email = email.getText().toString().isEmpty();
-        is_password = password.getText().toString().isEmpty();
+        boolean is_namesurname, is_username, is_email, is_password, is_age, is_weight, is_sex;
+        is_namesurname = binding.signupNamesurname.getText().toString().isEmpty();
+        is_username = binding.signupUsername.getText().toString().isEmpty();
+        is_email = binding.signupEmail.getText().toString().isEmpty();
+        is_password = binding.signupPassword.getText().toString().isEmpty();
+        is_age = binding.signupAge.getText().toString().isEmpty();
+        is_weight = binding.signupWeight.getText().toString().isEmpty();
+        is_sex = getSex().isEmpty();
 
-        if (is_username && is_namesurname && is_email && is_password)
+        if (is_username && is_namesurname && is_email && is_password && is_age && is_weight && is_sex)
             return false;
         else
             return true;
-
-
     }
 }
